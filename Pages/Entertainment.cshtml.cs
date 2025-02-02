14846e6e4736ddddc00c0c1e60ed4a515854699e
dev0xiinko <adrian.github.dev@gmail.com>
2025-02-02 22:34:08 +0800

    finalized the project

---
+++ Pages/Entertainment.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Alquizar_LabAct1.Pages;
+
+public class EntertainmentModel : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
